f2e6d3af87ccd9fd4c6eb99824bfe90686246b02
Matthew Hallquist <matthallquist@yahoo.com>
2020-04-03 23:30:45 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,6 +16,10 @@ def words_five_letters_long(string)
   string.scan(/\b\w{5}\b/)
 end
 
+def first_word_capitalized_and_ends_with_punctuation(text)
+  if text.match(/\b[A-Z][.,?!;:]\b/)
+end
+
 
 
 

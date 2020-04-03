5104b847cb0a0ea0bbba4a545559b5c3db71956b
Matthew Hallquist <matthallquist@yahoo.com>
2020-04-03 23:46:46 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,17 +8,16 @@ def starts_with_a_vowel?(word)
   end
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\bun\w*ing\b/)
-
+def words_starting_with_un_and_ending_with_ing(string)
+  string.scan(/\bun\w*ing\b/)
 end
 
-def words_five_letters_long(text)
-  text.scan(/\b\w{5}\b/)
+def words_five_letters_long(string)
+  string.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/([[:punct:]]$)/) && text.match(/^[A-Z]/)
+  if text.match(/\b[A-Z]\b/) && text.match(/[.,?!;:]\z/)
     true
   else
     false

0d8e33475b210716ba44078853b7156195df55e5
Matthew Hallquist <matthallquist@yahoo.com>
2020-04-03 23:22:46 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,14 +8,9 @@ def starts_with_a_vowel?(word)
   end
 end
 
-def words_starting_with_un_and_ending_with_ing
-  text.scan(/\bun\w*ing\b/)
+def words_starting_with_un_and_ending_with_ing(string)
+  string.scan(/\bun\w*ing\b/)
 end
-
-# def words_starting_with_un_and_ending_with_ing(text)
-#   text.scan(/\bun\w*ing\b/)
-#
-# end
 #
 # def words_five_letters_long(text)
 #   text.scan(/\b\w{5}\b/)
